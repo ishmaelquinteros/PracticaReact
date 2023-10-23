42300c3c3c922580e0d50b51676ef462a3a19193
ishmaelquinteros <113136817+ishmaelquinteros@users.noreply.github.com>
2023-10-23 15:28:14 -0300

    uso de State y Effect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { Carrito } from './Componentes/Carrito';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  
+  const [stock, setStock] = useState(1)
+
+  const AsignarStock = (propStock) => {
+        setStock(propStock);
+  };
+
+  useEffect (() => {
+    AsignarStock(5);
+  }, [])
+
+  const checkStock = (pedido, stockunidad) => {
+   if (pedido <= stockunidad){
+    setStock(stock - pedido);
+    <Carrito pedido={pedido}/>;
+    console.log("Se agrega " + pedido + " unidad/es al carrito");
+    console.log("Quedan en stock " + (stockunidad - pedido) + " unidad/es");
+  } else{
+    console.log("Lo sentimos no hay stock suficiente");
+  }  
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <button onClick={()=> setCount(count+1)}>+</button>
+    <label>{count}</label>
+    <button onClick={()=> setCount(count-1)}>-</button>
+    <button onClick={() => checkStock (count, stock)}>Agregar a carrito</button>
     </div>
   );
 }
---
+++ src/Componentes/Carrito.jsx
@@ -0,0 +1,10 @@
+
+export const Carrito = ({pedido}) => {
+  return (
+    <div>
+        <h3>Se agrego {pedido} unidad al carrito</h3>
+    </div>
+  )
+}
+
+export default Carrito;
